2dd579df5ec4934bb897ac1612bc689818335684
utkarsh bairolia <utkarsh.bairolia@nutanix.com>
2024-02-01 12:34:01 +0530

    Hackathon Update Main Page

--- my-react-project/src/App.js
+++ my-react-project/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hack to the End !!
+          Hack to the End v2.0 !!
         </p>
       </header>
     </div>
