5c9b5b2bf61d3943506eea65fa16cd72653b2584
utkarsh bairolia <utkarsh.bairolia@nutanix.com>
2024-02-01 09:54:39 +0530

    Update Main page Text

--- my-react-project/src/App.js
+++ my-react-project/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hack to the End 3rd Commit!!
+          Hack to the End !!
         </p>
       </header>
     </div>
